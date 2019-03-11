545afcbe39858b405b6501230bb62dfea63da537
Apple Market <alirezakara@Alirezas-MacBook-Pro.local>
2019-03-11 14:57:49 +0330

    Convert to MVC design pattern.

---
+++ ToDoList/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDoList
+//
+//  Created by Alireza Kara on 3/11/19.
+//  Copyright © 2019 Alireza Kara. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done: Bool = false
+    
+}
